249c0de04b0e816860478914203d81b1870162ec
MohamedMarwan53 <159703085+MohamedMarwan53@users.noreply.github.com>
2024-09-27 19:14:24 +0530

    First Commit

---
+++ crudwithexception/src/main/java/com/example/crudwithexception/CrudwithexceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudwithexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudwithexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudwithexceptionApplication.class, args);
+	}
+
+}
---
+++ crudwithexception/src/main/java/com/example/crudwithexception/entity/User.java
@@ -0,0 +1,55 @@
+
+package com.example.crudwithexception.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TableEmployee")
+public class User {
+	@Id
+	private int id;
+	private String name;
+	private String age;
+
+	
+	public User() {
+		
+	}
+
+	public User(int id, String name, String age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
